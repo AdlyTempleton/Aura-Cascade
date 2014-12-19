86221d5b9091bb6172bf8a6eacd3be764705c9c6
Pixlepix <adlytempleton@gmail.com>
2014-12-18 22:23:28 -0500

    Added dig fairy

---
+++ src/main/java/pixlepix/auracascade/block/entity/EntityDigFairy.java
@@ -0,0 +1,15 @@
+package pixlepix.auracascade.block.entity;
+
+import net.minecraft.block.material.Material;
+import net.minecraft.world.World;
+
+import java.util.Random;
+
+/**
+ * Created by pixlepix on 12/18/14.
+ */
+public class EntityDigFairy extends EntityFairy{
+    public EntityDigFairy(World p_i1582_1_) {
+        super(p_i1582_1_);
+    }
+}
--- src/main/java/pixlepix/auracascade/main/EventHandler.java
+++ src/main/java/pixlepix/auracascade/main/EventHandler.java
@@ -1,13 +1,22 @@
 package pixlepix.auracascade.main;
 
+import baubles.api.BaublesApi;
 import cpw.mods.fml.common.eventhandler.Event;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.entity.Entity;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.living.LivingSpawnEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import pixlepix.auracascade.block.entity.EntityBaitFairy;
+import pixlepix.auracascade.block.entity.EntityDigFairy;
+import pixlepix.auracascade.block.entity.EntityFairy;
 import pixlepix.auracascade.block.entity.EntityScareFairy;
+import pixlepix.auracascade.item.ItemFairyRing;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -28,6 +37,24 @@ public class EventHandler {
         if(scareCount > 0){
             if(random.nextInt(25) <= scareCount){
                 event.setResult(Event.Result.DENY);
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public void onGetBreakSpeed(PlayerEvent.BreakSpeed event){
+        ItemStack item = BaublesApi.getBaubles(event.entityPlayer).getStackInSlot(1);
+        if(event.block == Blocks.stone && item != null && item.getItem() instanceof ItemFairyRing && !event.entityPlayer.worldObj.isRemote){
+            List<EntityDigFairy> fairyList = event.entityPlayer.worldObj.getEntitiesWithinAABB(EntityDigFairy.class, event.entityPlayer.boundingBox.expand(20, 20, 20));
+            int count = -1;
+            for(EntityDigFairy digFairy:fairyList){
+                if(digFairy.player == event.entityPlayer){
+                    count ++;
+                }
+            }
+            count = Math.min(count, 15);
+            if(new Random().nextInt(50) <= count){
+                event.newSpeed = Float.MAX_VALUE;
             }
         }
     }
