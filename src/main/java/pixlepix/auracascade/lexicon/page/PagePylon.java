7ac908bab815eb2a9cd2a4d388f1317348df91e5
pixlepix <adlytempleton@gmail.com>
2015-01-17 09:56:53 -0500

    Fixed overlapping tooltips with the 'shift click to see recipe' and vortex infusion

--- src/main/java/pixlepix/auracascade/lexicon/page/PagePylon.java
+++ src/main/java/pixlepix/auracascade/lexicon/page/PagePylon.java
@@ -12,6 +12,7 @@ import pixlepix.auracascade.block.tile.CraftingCenterTile;
 import pixlepix.auracascade.data.AuraQuantity;
 import pixlepix.auracascade.data.EnumAura;
 import pixlepix.auracascade.data.recipe.PylonRecipe;
+import pixlepix.auracascade.data.recipe.PylonRecipeRegistry;
 import pixlepix.auracascade.lexicon.IGuiLexiconEntry;
 import pixlepix.auracascade.lexicon.LexiconEntry;
 import pixlepix.auracascade.lexicon.LexiconRecipeMappings;
@@ -76,8 +77,13 @@ public class PagePylon extends PageRecipe {
             int xPos = gui.getLeft() + x * 29 + 7 + (y == 0 && x == 3 ? 10 : 0);
             int yPos = gui.getTop() + y * 29 + 24 - (y == 0 ? 7 : 0);
             AuraQuantity quantity = recipe.componentList.get(i).auraQuantity;
+            //If the 'Shift click to see recipe is present
+            //We need to offset the aura display
+
+            LexiconRecipeMappings.EntryData data = LexiconRecipeMappings.getDataForStack(recipe.componentList.get(i).itemStack);
+            int yOffset = (data != null && (data.entry != gui.getEntry() || data.page != gui.getPageOn())) ? 32 : 16;
             if (mx >= xPos && my >= yPos && mx < xPos + 16 && my < yPos + 16) {
-                VazkiiRenderHelper.renderTooltip(mx, my + 16, Arrays.asList("" + quantity.getNum() + "(" + (quantity.getType() == EnumAura.WHITE_AURA ? "Any" : quantity.getType().name) + ")"));
+                VazkiiRenderHelper.renderTooltip(mx, my + yOffset, Arrays.asList("" + quantity.getNum() + "(" + (quantity.getType() == EnumAura.WHITE_AURA ? "Any" : quantity.getType().name) + ")"));
             }
         }
         renderItemAtGridPos(gui, 2, 0, recipe.result, false);
