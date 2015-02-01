be1354af31562b744fdf89537cbd99308f840605
pixlepix <adlytempleton@gmail.com>
2015-01-31 19:19:45 -0500

    Removed artifact on slot render

--- src/main/java/pixlepix/auracascade/gui/SlotCoordinator.java
+++ src/main/java/pixlepix/auracascade/gui/SlotCoordinator.java
@@ -32,54 +32,6 @@ public class SlotCoordinator extends Slot {
         super(te, p_i1824_2_, p_i1824_3_, p_i1824_4_);
         this.storage = storageItemStack;
         this.te = te;
-        this.setBackgroundIcon(new IIcon() {
-            @Override
-            public int getIconWidth() {
-                return 16;
-            }
-
-            @Override
-            public int getIconHeight() {
-                return 16;
-            }
-
-            @Override
-            public float getMinU() {
-                return 176;
-            }
-
-            @Override
-            public float getMaxU() {
-                return 176 + 16;
-            }
-
-            @Override
-            public float getInterpolatedU(double d) {
-                return (float) (176F + d * 16F);
-            }
-
-            @Override
-            public float getMinV() {
-                return 166;
-            }
-
-            @Override
-            public float getMaxV() {
-                return 166 + 16;
-            }
-
-            @Override
-            public float getInterpolatedV(double d) {
-
-                return (float) (166F + d * 16F);
-            }
-
-            @Override
-            public String getIconName() {
-                return "slot";
-            }
-        });
-        this.setBackgroundIconTexture(new ResourceLocation("aura", "/textures/gui/coordinator.png"));
 
     }
 
