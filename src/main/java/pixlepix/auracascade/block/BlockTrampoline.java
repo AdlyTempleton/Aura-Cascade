7afa9109c81bee4bdc527db46bacc2549b27146b
pixlepix <adlytempleton@gmail.com>
2015-02-05 21:11:05 -0500

    Added a trampoline. Yes, you heard me right.

---
+++ src/main/java/pixlepix/auracascade/block/BlockTrampoline.java
@@ -0,0 +1,84 @@
+package pixlepix.auracascade.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.ITileEntityProvider;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.Entity;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Vec3;
+import net.minecraft.world.World;
+import pixlepix.auracascade.data.AuraQuantity;
+import pixlepix.auracascade.data.EnumAura;
+import pixlepix.auracascade.data.recipe.PylonRecipe;
+import pixlepix.auracascade.data.recipe.PylonRecipeComponent;
+import pixlepix.auracascade.registry.ITTinkererBlock;
+import pixlepix.auracascade.registry.ThaumicTinkererRecipe;
+
+import java.util.ArrayList;
+
+/**
+ * Created by localmacaccount on 2/5/15.
+ */
+public class BlockTrampoline extends Block implements ITTinkererBlock {
+    public BlockTrampoline() {
+        super(Material.cloth);
+        setBlockBounds(0F, 0F, 0F, 1F, .8F, 1F);
+    }
+
+    @Override
+    public ArrayList<Object> getSpecialParameters() {
+        return null;
+    }
+
+    @Override
+    public String getBlockName() {
+        return "trampoline";
+    }
+
+    @Override
+    public void onEntityCollidedWithBlock(World world, int x, int y, int z, Entity entity) {
+        entity.addVelocity(0, 10, 0);
+
+    }
+
+    @Override
+    public boolean shouldRegister() {
+        return true;
+    }
+
+    @Override
+    public void registerBlockIcons(IIconRegister register) {
+        blockIcon = register.registerIcon("aura:trampoline");
+    }
+
+    @Override
+    public boolean isOpaqueCube() {
+        return false;
+    }
+
+    @Override
+    public boolean shouldDisplayInTab() {
+        return true;
+    }
+
+    @Override
+    public Class<? extends ItemBlock> getItemBlock() {
+        return null;
+    }
+
+    @Override
+    public Class<? extends TileEntity> getTileEntity() {
+        return null;
+    }
+
+    @Override
+    public ThaumicTinkererRecipe getRecipeItem() {
+        return new PylonRecipe(new ItemStack(this, 16), new PylonRecipeComponent(new AuraQuantity(EnumAura.WHITE_AURA, 10000),
+                new ItemStack(Items.slime_ball)));
+    }
+}
