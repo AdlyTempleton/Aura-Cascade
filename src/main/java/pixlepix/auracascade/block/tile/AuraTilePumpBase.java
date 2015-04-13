29a2eaf58df950fd74e6935fbce8dffad216210e
pixlepix <adlytempleton@gmail.com>
2015-04-13 18:07:46 -0400

    Lowered alternating pump buff to x3 from x5

--- src/main/java/pixlepix/auracascade/block/tile/AuraTilePumpBase.java
+++ src/main/java/pixlepix/auracascade/block/tile/AuraTilePumpBase.java
@@ -46,7 +46,7 @@ public class AuraTilePumpBase extends AuraTile {
             pumpSpeed = speed;
             pumpPower = time;
             if(isAlternator()){
-                pumpSpeed *=5;
+                pumpSpeed *= 3;
                 
             }
         }
