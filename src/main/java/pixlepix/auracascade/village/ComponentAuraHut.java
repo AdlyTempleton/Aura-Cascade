f448e8761cc99d672ba1196a56f63c6862c43f4b
pixlepix <adlytempleton@gmail.com>
2015-05-26 18:05:10 -0400

    Fixed potential issues with aura huts

--- src/main/java/pixlepix/auracascade/village/ComponentAuraHut.java
+++ src/main/java/pixlepix/auracascade/village/ComponentAuraHut.java
@@ -17,14 +17,27 @@ import java.util.Random;
  */
 public class ComponentAuraHut extends StructureVillagePieces.WoodHut {
 
-    public boolean addComponentParts(World p_74875_1_, Random p_74875_2_, StructureBoundingBox p_74875_3_)
+    public ComponentAuraHut(StructureVillagePieces.Start p_i2099_1_, int p_i2099_2_, Random p_i2099_3_, StructureBoundingBox p_i2099_4_, int p_i2099_5_)
     {
-        if (this.field_143015_k < 0)
-        {
+        super(p_i2099_1_, p_i2099_2_, p_i2099_3_, p_i2099_4_, p_i2099_5_);
+    }
+
+
+    public ComponentAuraHut() {
+        super();
+
+    }
+
+    public static ComponentAuraHut buildComponent (StructureVillagePieces.Start villagePiece, List pieces, Random random, int p1, int p2, int p3, int p4, int p5) {
+        StructureBoundingBox structureboundingbox = StructureBoundingBox.getComponentToAddBoundingBox(p1, p2, p3, 0, 0, 0, 7, 6, 7, p4);
+        return canVillageGoDeeper(structureboundingbox) && StructureComponent.findIntersecting(pieces, structureboundingbox) == null ? new ComponentAuraHut(villagePiece, p5, random, structureboundingbox, p4) : null;
+    }
+
+    public boolean addComponentParts(World p_74875_1_, Random p_74875_2_, StructureBoundingBox p_74875_3_) {
+        if (this.field_143015_k < 0) {
             this.field_143015_k = this.getAverageGroundLevel(p_74875_1_, p_74875_3_);
 
-            if (this.field_143015_k < 0)
-            {
+            if (this.field_143015_k < 0) {
                 return true;
             }
 
@@ -64,22 +77,19 @@ public class ComponentAuraHut extends StructureVillagePieces.WoodHut {
         this.placeBlockAtCurrentPosition(p_74875_1_, Blocks.air, 0, 1, 2, 0, p_74875_3_);
         this.placeDoorAtCurrentPosition(p_74875_1_, p_74875_3_, p_74875_2_, 1, 1, 0, this.getMetadataWithOffset(Blocks.wooden_door, 1));
 
-        if (this.getBlockAtCurrentPosition(p_74875_1_, 1, 0, -1, p_74875_3_).getMaterial() == Material.air && this.getBlockAtCurrentPosition(p_74875_1_, 1, -1, -1, p_74875_3_).getMaterial() != Material.air)
-        {
+        if (this.getBlockAtCurrentPosition(p_74875_1_, 1, 0, -1, p_74875_3_).getMaterial() == Material.air && this.getBlockAtCurrentPosition(p_74875_1_, 1, -1, -1, p_74875_3_).getMaterial() != Material.air) {
             this.placeBlockAtCurrentPosition(p_74875_1_, Blocks.stone_stairs, this.getMetadataWithOffset(Blocks.stone_stairs, 3), 1, 0, -1, p_74875_3_);
         }
 
-        for (int i = 0; i < 5; ++i)
-        {
-            for (int j = 0; j < 4; ++j)
-            {
+        for (int i = 0; i < 5; ++i) {
+            for (int j = 0; j < 4; ++j) {
                 this.clearCurrentPositionBlocksUpwards(p_74875_1_, j, 6, i, p_74875_3_);
                 this.func_151554_b(p_74875_1_, Blocks.cobblestone, 0, j, -1, i, p_74875_3_);
             }
         }
 
         this.spawnVillagers(p_74875_1_, p_74875_3_, 1, 1, 2, 1);
-        
+
         //Begin Aura Cascade specific setup
         this.placeBlockAtCurrentPosition(p_74875_1_, AuraBlock.getBlockFromName("pump"), 0, 2, 1, 3, p_74875_3_);
         this.placeBlockAtCurrentPosition(p_74875_1_, AuraBlock.getBlockFromName(""), 0, 2, 1, 2, p_74875_3_);
@@ -89,16 +99,5 @@ public class ComponentAuraHut extends StructureVillagePieces.WoodHut {
 
         this.placeBlockAtCurrentPosition(p_74875_1_, ConsumerBlock.getBlockFromName("furnace"), 0, 2, 1, 1, p_74875_3_);
         return true;
-    }
-
-
-    public ComponentAuraHut(StructureVillagePieces.Start p_i2099_1_, int p_i2099_2_, Random p_i2099_3_, StructureBoundingBox p_i2099_4_, int p_i2099_5_)
-    {
-        super(p_i2099_1_, p_i2099_2_, p_i2099_3_, p_i2099_4_, p_i2099_5_);
-    }
-
-    public static ComponentAuraHut buildComponent (StructureVillagePieces.Start villagePiece, List pieces, Random random, int p1, int p2, int p3, int p4, int p5) {
-        StructureBoundingBox structureboundingbox = StructureBoundingBox.getComponentToAddBoundingBox(p1, p2, p3, 0, 0, 0, 7, 6, 7, p4);
-        return canVillageGoDeeper(structureboundingbox) && StructureComponent.findIntersecting(pieces, structureboundingbox) == null ? new ComponentAuraHut(villagePiece, p5, random, structureboundingbox, p4) : null;
     }
 }
