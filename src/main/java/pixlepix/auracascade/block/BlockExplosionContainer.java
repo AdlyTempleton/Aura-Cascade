5874e9248d0d6818d377522e5cf2948e0be4e096
pixlepix <adlytempleton@gmail.com>
2015-06-06 14:24:31 -0400

    Added and tweaked repair

--- src/main/java/pixlepix/auracascade/block/BlockExplosionContainer.java
+++ src/main/java/pixlepix/auracascade/block/BlockExplosionContainer.java
@@ -6,11 +6,13 @@ import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.IIcon;
+import net.minecraft.world.World;
 import pixlepix.auracascade.AuraCascade;
 import pixlepix.auracascade.registry.ITTinkererBlock;
 import pixlepix.auracascade.registry.ThaumicTinkererRecipe;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by localmacaccount on 6/5/15.
@@ -25,11 +27,37 @@ public class BlockExplosionContainer extends Block implements ITTinkererBlock {
         //Same as obby
         setResistance(2000F);
         type = "Dirt";
+        setTickRandomly(true);
     }
 
     public BlockExplosionContainer(String s) {
         this();
         type = s;
+    }
+
+    @Override
+    public int onBlockPlaced(World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ, int meta) {
+        super.onBlockPlaced(world, x, y, z, side, hitX, hitY, hitZ, meta);
+        world.scheduleBlockUpdate(x, y, z, this, tickRate(world));
+        return meta;
+    }
+
+    @Override
+    public int tickRate(World world) {
+        return 300;
+    }
+
+    @Override
+    public void updateTick(World world, int x, int y, int z, Random rand) {
+        super.updateTick(world, x, y, z, rand);
+        if (rand.nextDouble() < getChanceToRepair()) {
+            int meta = world.getBlockMetadata(x, y, z);
+            if (meta > 0) {
+                world.setBlockMetadataWithNotify(x, y, z, meta - 1, 3);
+
+            }
+        }
+        world.scheduleBlockUpdate(x, y, z, this, tickRate(world) + rand.nextInt(5));
     }
 
     /**
@@ -69,29 +97,29 @@ public class BlockExplosionContainer extends Block implements ITTinkererBlock {
             return .25;
         }
         if (type.equals("Obsidian")) {
-            return .9;
+            return .99;
         }
         return 0;
     }
 
     public double getChanceToRepair() {
         if (type.equals("Dirt")) {
-            return .25;
-        }
-        if (type.equals("Wood")) {
-            return .9;
-        }
-        if (type.equals("Glass")) {
-            return .75;
-        }
-        if (type.equals("Cobblestone")) {
-            return .75;
-        }
-        if (type.equals("Stone")) {
             return .5;
         }
+        if (type.equals("Wood")) {
+            return .75;
+        }
+        if (type.equals("Glass")) {
+            return .25;
+        }
+        if (type.equals("Cobblestone")) {
+            return .5;
+        }
+        if (type.equals("Stone")) {
+            return .25;
+        }
         if (type.equals("Obsidian")) {
-            return .1;
+            return .05;
         }
         return 0;
     }
