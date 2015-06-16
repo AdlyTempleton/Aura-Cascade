9ba56017413404bd442f10710ef733a6d9c73ff7
pixlepix <adlytempleton@gmail.com>
2015-06-16 18:58:09 -0400

    Removed phantom ore. Not nessacary design-wise

--- src/main/java/pixlepix/auracascade/block/entity/EntityMinerExplosion.java
+++ src/main/java/pixlepix/auracascade/block/entity/EntityMinerExplosion.java
@@ -6,7 +6,6 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
 import pixlepix.auracascade.block.BlockExplosionContainer;
-import pixlepix.auracascade.block.BlockPhantomOre;
 
 import java.util.Random;
 
@@ -92,9 +91,6 @@ public class EntityMinerExplosion extends Entity {
                                     worldObj.setBlockMetadataWithNotify(xCoord + i, yCoord + j, zCoord + k, meta, 3);
                                 }
                             }
-                        }
-                        if (block instanceof BlockPhantomOre) {
-                            contained = true;
                         }
                     }
                 }
