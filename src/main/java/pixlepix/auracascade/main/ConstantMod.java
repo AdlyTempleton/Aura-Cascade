e5f35af2a16dc4b1b9bd1cf28960293d605ae360
pixlepix <adlytempleton@gmail.com>
2015-08-06 20:35:16 -0400

    Fixed secret key replacement

--- src/main/java/pixlepix/auracascade/main/ConstantMod.java
+++ src/main/java/pixlepix/auracascade/main/ConstantMod.java
@@ -8,7 +8,7 @@ public class ConstantMod {
 
     public static final String version = "@version@";
     public static final String analyticsKey = "18bb8a5cfcbc8e0f3801148ac1277152";
-    public static final String analyticsKeySecret = "@keySecret";
+    public static final String analyticsKeySecret = "@keySecret@";
     
 
 
