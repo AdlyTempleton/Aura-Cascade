ea17db97bd94e188a58da026036ff06d6db336c6
Vincent Lee <vincent.lee@outlook.com>
2015-12-13 10:32:10 -0600

    Preserving my sanity. This is it.

--- src/main/java/pixlepix/auracascade/ModelHandler.java
+++ src/main/java/pixlepix/auracascade/ModelHandler.java
@@ -1,16 +1,23 @@
 package pixlepix.auracascade;
 
-import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.registry.GameData;
+import pixlepix.auracascade.block.AuraBlock;
+import pixlepix.auracascade.registry.BlockRegistry;
 
 public final class ModelHandler {
 
     public static void registerModels() {
-
+        registerItem(Item.getItemFromBlock(BlockRegistry.getBlockFromClassAndName(AuraBlock.class, "auraNode")));
     }
 
     private static void registerItem(Item item) {
+        ResourceLocation l = GameData.getItemRegistry().getNameForObject(item);
+        System.out.println(l);
+        ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(l, "inventory"));
     }
 
     private ModelHandler() {}
