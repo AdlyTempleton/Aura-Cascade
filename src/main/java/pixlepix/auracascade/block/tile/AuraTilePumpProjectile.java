66e17782fb8e067aaa0f0b73ba8c808da54bcfd3
Soullesswaffle <mickeydekkers@gmail.com>
2016-04-19 20:39:15 +0200

    Fix Projectile Pumps not reacting to arrows

--- src/main/java/pixlepix/auracascade/block/tile/AuraTilePumpProjectile.java
+++ src/main/java/pixlepix/auracascade/block/tile/AuraTilePumpProjectile.java
@@ -4,7 +4,6 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.projectile.EntityArrow;
 import net.minecraft.entity.projectile.EntityEgg;
 import net.minecraft.entity.projectile.EntitySnowball;
-import net.minecraft.entity.projectile.EntityThrowable;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.EnumParticleTypes;
 import pixlepix.auracascade.main.Config;
@@ -19,7 +18,6 @@ public class AuraTilePumpProjectile extends AuraTilePumpBase {
     public void onEntityCollidedWithBlock(Entity entity) {
         if (entity instanceof EntityArrow) {
             addFuel(Config.pumpArrowDuration, Config.pumpArrowSpeed);
-            entity.setDead();
         }
 
         if (entity instanceof EntityEgg) {
@@ -34,13 +32,18 @@ public class AuraTilePumpProjectile extends AuraTilePumpBase {
     @Override
     public void update() {
         super.update();
-        List<EntityThrowable> entityList = worldObj.getEntitiesWithinAABB(EntityThrowable.class, new AxisAlignedBB(pos.getX() - .5, pos.getY() - .5, pos.getZ() - .5, pos.getX() + 1.5, pos.getY() + 1.5, pos.getZ() + 1.5));
+        List<Entity> entityList = worldObj.getEntitiesWithinAABB(Entity.class, new AxisAlignedBB(pos.getX() - .5, pos.getY() - .5, pos.getZ() - .5, pos.getX() + 1.5, pos.getY() + 1.5, pos.getZ() + 1.5));
         for (Entity entity : entityList) {
             if (entity instanceof EntitySnowball || entity instanceof EntityEgg) {
                 //Fun fact: Eggs and snowballs use the same particle code
                 for (int i = 0; i < 8; ++i) {
                     this.worldObj.spawnParticle(EnumParticleTypes.SNOWBALL, entity.posX, entity.posY, entity.posZ, 0.0D, 0.0D, 0.0D);
                 }
+                entity.setDead();
+                if (!worldObj.isRemote) {
+                    onEntityCollidedWithBlock(entity);
+                }
+            } else if (entity instanceof EntityArrow) {
                 entity.setDead();
                 if (!worldObj.isRemote) {
                     onEntityCollidedWithBlock(entity);
