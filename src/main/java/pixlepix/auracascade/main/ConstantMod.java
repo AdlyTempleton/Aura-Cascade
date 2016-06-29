58fa71daf5b5e453621b7a606f627ebf2a10565f
Adly Templeton <adlytempleton@gmail.com>
2016-06-28 22:08:31 -0400

    Revert "Removed VersionChecker"
    This reverts commit 301f93cd2449de502f7789f353434172f59c1560.

--- src/main/java/pixlepix/auracascade/main/ConstantMod.java
+++ src/main/java/pixlepix/auracascade/main/ConstantMod.java
@@ -8,6 +8,8 @@ public class ConstantMod {
 
     public static final String version = "593";
 
+    public static final String URL = "https://raw.githubusercontent.com/DrasticDemise/Aura-Cascade/MC188/1.9%20Update%20Handler";
+
     public static final String clientProxy = "pixlepix.auracascade.main.ClientProxy";
     public static final String commonProxy = "pixlepix.auracascade.main.CommonProxy";
 
